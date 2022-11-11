502701b544f8ffe42a53825174d8f0096b5b3c4d
Salies <serezane@protonmail.com>
2022-11-11 20:15:33 -0300

    timeout working

---
+++ sketch_oct27a/sketch_oct27a.ino
@@ -0,0 +1,42 @@
+#define TIME 300
+unsigned short timeout[2] = {TIME, TIME};
+bool counting[2] = {false, false};
+  
+void setup()
+{
+  //Define a porta do led como saida
+  Serial.begin(9600);
+  //pinMode(11, OUTPUT);
+  //pinMode(3, INPUT);
+}
+  
+void loop()
+{
+  // se o timeout já estiver esgotado, reseta e conta de novo
+  if(analogRead(A2) > 400 && timeout[0] >= TIME) {
+    Serial.print("A");
+    timeout[0] = 0;
+    counting[0] = true;
+  }
+
+  // está no timeout
+  if(counting[0] && timeout[0] < TIME) {
+    timeout[0]++;
+  }
+
+  // timeout deu 100, para de contar
+  if(counting[0] && timeout[0] >= TIME) {
+    counting[0] = false;
+  }
+
+
+  //Acende o led
+    
+  /*if(analogRead(A2) == HIGH){
+    digitalWrite(11, HIGH);
+    Serial.print("A");
+  }
+  else{
+    digitalWrite(11, LOW);
+  }*/
+}
--- teste.py
+++ teste.py
@@ -1,10 +1,11 @@
 import serial
+from time import sleep
 #import pydirectinput
 
-ser = serial.Serial('COM4', 9600, timeout=1)
+ser = serial.Serial('COM3', 9600, timeout=1)
 
 while True:
-    if ser.in_waiting:
+    if ser.in_waiting and not trigger:
         msg = ser.read().decode('utf-8')
         print(msg)
         #if(msg == "A"):
